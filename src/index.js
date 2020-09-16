d28580d093f52b2b07c86d09b068b40c4ed89814
Ganesh Koilada <itsmeganeshcse@gmail.com>
2020-09-16 10:52:30 +0530

    Update index.js
    Remove bootstrap dependency

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./redux/store";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
